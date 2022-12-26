650512131c782872c0de8172b8165b632048e0d4
unknown <baggamehak24@gmail.com>
2022-12-26 22:29:50 +0530

    Check Palindrome Approach 2

--- Linked list/5.CheckPalindrome.cpp
+++ Linked list/5.CheckPalindrome.cpp
@@ -59,6 +59,63 @@ bool isPalindrome(Node* head){
 	}
 	return checkPalindrome(arr);
 }
+
+
+//APPROACH 2
+Node* middle(Node* head){
+	Node* slow=head;
+	Node* fast=head->next;
+	
+	while(fast!=NULL && fast->next!=NULL){
+		fast=fast->next->next;
+		slow=slow->next;
+	}
+	return slow;
+}
+Node* reverse(Node* head){
+	Node* curr=head;
+	Node* prev=NULL;
+	Node* forward=NULL;
+	
+	while(curr!=NULL && curr->next!=NULL){
+		forward=curr->next;
+		curr->next=prev;
+		prev=curr;
+		curr=forward;
+	}
+	return prev;
+}
+bool isPalindrome2(Node* head){
+	if(head->next==NULL){
+		return true;
+	}
+	
+	//step1: find middle
+	Node* mid=middle(head);
+	
+	//step2: reverse the lL after middle element
+	Node* temp=mid->next;
+	mid->next=reverse(head);
+	
+	//step3: compare both the halves
+    Node* head1=head;
+    Node* head2=mid->next;
+    
+    while(head2!=NULL){
+    	if(head1->data!=head2->data){
+		   return false;
+	}
+		head1 = head1->next;
+		head2 = head2->next;
+	}
+	
+	//step 4: reverse the LL again 
+    temp=mid->next;
+    mid->next=reverse(head);
+     
+    return true;
+}
+
 int main()
 {
   Node* head=takeinput();
