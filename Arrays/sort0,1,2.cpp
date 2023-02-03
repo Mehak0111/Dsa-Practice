6791b257a315b5a9c35695fb5bb54c7cdadb16c5
unknown <baggamehak24@gmail.com>
2023-02-03 15:46:42 +0530

    sort 0's 1's and 2's

---
+++ Arrays/sort0,1,2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int arr[100];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int low=0,mid=0,high=n-1;
+	while(mid<=high){
+		if(arr[mid]==0){
+			swap(arr[low],arr[mid]);
+			low++;
+			mid++;
+		}
+		else if(arr[mid==2]){
+			swap(arr[high],arr[mid]);
+			high--;
+		}
+		else{
+			mid++;
+		}
+	}
+	
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+}
