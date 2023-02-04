f79be3297c8110818451259ea37b4a10758ddfe1
unknown <baggamehak24@gmail.com>
2023-02-04 11:19:55 +0530

    reverse array method 2

--- Arrays/reverse.cpp
+++ Arrays/reverse.cpp
@@ -8,7 +8,19 @@ int main()
 	for(int i=0;i<n;i++){
 		cin>>arr[i];
 	}
-	for(int i=n-1;i>=0;i--){
+//	for(int i=n-1;i>=0;i--){
+//		cout<<arr[i]<<" ";
+//	}
+
+    int start=0;
+    int end=n-1;
+    while(start<end){
+    	swap(arr[start],arr[end]);
+    	start++;
+    	end--;
+	}
+	
+	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
 }
