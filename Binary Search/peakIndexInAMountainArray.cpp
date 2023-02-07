ea3aae9ecd6140f149422d60f7f1dc096211b7c5
unknown <baggamehak24@gmail.com>
2023-02-07 10:56:44 +0530

    find peak index in a mountain array

---
+++ Binary Search/peakIndexInAMountainArray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int peakIndex(int arr[],int n){
+   int start=0;
+   int end=n-1;
+   int mid=(start+end)/2;
+   while(start<end){
+   	if(arr[mid]<arr[mid+1]){
+   		start=mid+1;
+	   }
+	else{
+		end=mid;
+	}
+	mid=(start+end)/2;
+   }
+   return arr[start];
+}
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	cout<<peakIndex(arr,n);
+}
