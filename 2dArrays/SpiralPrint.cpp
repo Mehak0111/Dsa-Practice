61d8ff48f718d2e26d164e8f51068de3a3bc177b
unknown <baggamehak24@gmail.com>
2023-02-15 13:16:59 +0530

    spiral print

---
+++ 2dArrays/SpiralPrint.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,m;
+	cin>>n>>m;
+	int arr[n][m];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cin>>arr[i][j];
+		}
+	}
+	int count=0;
+	int total=n*m;
+	int startrow=0;
+	int startcol=0;
+	int endrow=n-1;
+	int endcol=m-1;
+	while(count<total){
+		
+}
+}
